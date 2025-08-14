0ce63b83adcf95f52fb276fc0432a5563fcdd433
MiguelLewandowski <miguelstrapazonl@gmail.com>
2025-08-14 16:35:01 -0300

    feat: add DepsModels component to home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero';
 import Portfolio from '@/components/Portfolio';
 import Services from '@/components/Services';
+import DepsModels from '@/components/DepsModels';
 import About from '@/components/About';
 import FAQ from '@/components/FAQ';
 import Footer from '@/components/Footer';
@@ -17,6 +18,7 @@ export default function Home() {
       </BackgroundWrapper>
     
       <Services />
+      <DepsModels />
       <About />
       <FAQ />
       <Footer />
